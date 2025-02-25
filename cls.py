90e04a60bc1bb09a895035ba6eef9a38cf10dda4
Kirpich-12 <ponchik34@tuta.io>
2025-02-25 13:20:57 +0300

    + FLY

--- cls.py
+++ cls.py
@@ -18,14 +18,21 @@ class Birds:
     def sleping(self):
         print('zzzz')
     
+    def flying_range(self, n):
+        if self.flying_ability is False:
+            print(f"{self.name} can't fly")
+        else:
+            print(f'{self.name} can fly {self.size * n}')
+
     def __del__(self):
         print(f'{self.name} has delited')
 
 
 def tst():
     pts = Birds('Petux', False, 15)
-    pts.eating()
-    print(type(pts))
+    pts.flying_range(4)
+    pts2 = Birds('Nightingale', True, 15)
+    pts2.flying_range(4)
 
 
 def main():
